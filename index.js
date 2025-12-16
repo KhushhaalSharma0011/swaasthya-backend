271085e454af2285058cd2a568bf25d38cdaec30
KhushhaalSharma0011 <165359267+KhushhaalSharma0011@users.noreply.github.com>
2025-12-16 20:14:07 +0530

    Fix port for Render

--- index.js
+++ index.js
@@ -52,4 +52,7 @@ User: ${req.body.message}
   });
 });
 
-app.listen(3000, () => console.log("Server running"));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log("Server running on port", PORT));
+
+
